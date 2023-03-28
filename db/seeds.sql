7f424fa3596238c78d06f52dd7e11bcb260eaf7b
Allyson Gonzales <Allysonmg21@gmail.com>
2023-03-27 22:28:33 -0700

    created seeds.sql and formatted

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,27 @@ INSERT INTO department (name)
 VALUES ("Finance");
 INSERT INTO department(name)
 VALUES ("Legal");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead, 80000", 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer, 150000", 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Account Manager, 160000", 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead, 250000", 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chan", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ashley", "Rodriguez", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tupik", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kunal", "Singh", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Malia", "Brown", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sarah", "Lourd", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Allyson", "Gonzales", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Allen", 1, 2);
